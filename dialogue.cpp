6b044664baab62c698b430259b9a965cb6f07cd8
Allison <allisonpa16@gmail.com>
2025-02-15 14:54:50 -0500

    dialogue file uploaded

---
+++ dialogue.cpp
@@ -0,0 +1,19 @@
+//hw making dialogue with different color lines
+
+#include <ncurses.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main(int argc, char ** argv){
+	initscr();
+	start_color();
+	
+
+
+	getch();
+	endwin();
+
+	return 0;
+}
